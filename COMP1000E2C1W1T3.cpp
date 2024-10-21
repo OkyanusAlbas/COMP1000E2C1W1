34c0cf2beb6c9b909bcabcc9c2c2e04693b46711
OkyanusAlbas <albaokya@icloud.com>
2024-10-21 21:50:38 +0100

    Add files via upload

---
+++ COMP1000E2C1W1T3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int numbers[5] = {5, 10, 15, 20, 25}; // Declare and initialize the array
+
+    cout << "The third element in the array is: " << numbers[2] << endl; // Access and print the third element (index 2)
+
+    return 0;
+}
